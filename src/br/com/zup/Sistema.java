bb070d5f86888ebcf45d941c06199ba9d7df865b
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-10-08 12:09:47 -0300

    Modelar menu na Classe Sistema.

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -4,25 +4,59 @@ import java.util.Scanner;
 
 public class Sistema {
 
-    private static Scanner capturarDados(String mensagem){
+    private static Scanner capturarDados(String mensagem) {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
 
+    private static void menuPrincipal() {
+        System.out.println("Digite 1: Para Cadastrar uma Venda");
+        System.out.println("Digite 2: Para Mostrar as Vendas Cadastradas");
+        System.out.println("Digite 3: Para Sair do Menu");
+    }
 
-    private static Cliente cadastrarClientes(){
+    private static Cliente cadastrarClientes() {
         String nome = capturarDados("Informe o nome do Cliente: ").nextLine();
         String cpf = capturarDados("Informe o CPF do " + nome + ":").nextLine();
         String email = capturarDados("Informe o email do " + nome + ":").nextLine();
 
-        return ServicoCliente.cadastrarClientes(nome,cpf,email);
+        return ServicoCliente.cadastrarClientes(nome, cpf, email);
     }
 
-    private static VendedorResponsavel cadastrarVendedores(){
-        String nome = capturarDados("Informe o nome do Vendedor: ").nextLine();
+    private static VendedorResponsavel cadastrarVendedores() {
+        String nome = capturarDados("Informe o nome do Vendedor:").nextLine();
         String cpf = capturarDados("Informe o CPF do " + nome + ":").nextLine();
         String email = capturarDados("Informe o email do " + nome + ":").nextLine();
 
-        return ServicoVendedor.cadastrarVendedor(nome,cpf,email);
+        return ServicoVendedor.cadastrarVendedor(nome, cpf, email);
     }
+
+    private static Venda cadastrarVendas() {
+        double valorASerPago = capturarDados("Informe o valor a ser pago:").nextDouble();
+        String dataDeRegistro = capturarDados("Informe a Data De Registro da Venda: Ex dd/mm/AAAA").nextLine();
+        Cliente cliente = cadastrarClientes();
+        VendedorResponsavel vendedorResponsavel = cadastrarVendedores();
+
+        return ServicoVenda.cadastrarVendas(vendedorResponsavel, cliente, valorASerPago, dataDeRegistro);
+    }
+
+    public static void executar() {
+        boolean continuarMenu = true;
+
+        while (continuarMenu) {
+
+            menuPrincipal();
+            int opcao = capturarDados("Informe a sua opção escolhida:").nextInt();
+
+            if(opcao == 1){
+
+            }else if(opcao == 2){
+
+            }else {
+                continuarMenu = false;
+            }
+
+        }
+    }
+
 }
